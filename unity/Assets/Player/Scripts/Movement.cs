2455e17707acebf1840ca30ac0ff9874225b9b6e
MegaGames03 <chucky122003@gmail.com>
2022-04-08 16:50:33 -0700

    Axe throwing and Improve movement
    The axe throwing is really buggy soo

--- unity/Assets/Player/Scripts/Movement.cs
+++ unity/Assets/Player/Scripts/Movement.cs
@@ -4,30 +4,40 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float sensitivity;
-    public float Speed;
     public Transform Camera;
-    private Rigidbody rb;
+    public Rigidbody rb;
+    public float Speed;
+    public float sensitivity;
+    public float JumpPower;
 
+    private float MouseX;
     private float MouseY;
+
     private float MaxSpeed;
     private bool Grounded;
+    private bool Jumping;
+    private bool CanJump;
+    private float JumpCool;
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Checks for ground and than if it was found reduce the JumpCool
+        GroundCheck();
+
+        //Calls the Move Function
         Move();
+
+        //Calls the CameraMove FUnction
         RotatePlayer();
     }
 
-    //Movement function
     void Move()
     {
         //Intakes the horizontal and vertical axis to move the player and sets those vaules to a vector3 that points where the player is facing
@@ -79,5 +89,53 @@ public class Movement : MonoBehaviour
         //Sets the camera after all the movement has been done
         Camera.position = transform.position;
         Camera.eulerAngles = new Vector3(MouseY, transform.eulerAngles.y, 0);
+    }
+
+    //Checks the ground and checks if you can jump
+    void GroundCheck()
+    {
+        //Checks the ground by having a collider array and checking the tag of each collider
+        Collider[] GroundCheck = Physics.OverlapSphere(transform.position - new Vector3(0, 1f, 0), 0.25f);
+
+        //Checks if any of the colliders was the ground 
+        Grounded = false;
+        foreach (Collider col in GroundCheck)
+        {
+            if (col.gameObject.tag == "Ground")
+                Grounded = true;
+        }
+
+        if (Grounded == true)
+            if (JumpCool > 0)
+            {
+                JumpCool -= Time.deltaTime;
+                CanJump = false;
+            }
+            else
+                CanJump = true;
+
+        //Jumps the player if they can 
+        if (Input.GetButton("Jump") && CanJump == true)
+        {
+            CanJump = false;
+            StartCoroutine(Jump());
+        }
+    }
+
+    //Jumps 
+    IEnumerator Jump()
+    {
+        //Adds force 10 times quickly for a clean jump
+        for (int i = 0; i < 5; i++)
+        {
+            rb.AddForce(new Vector3(0, JumpPower * Speed, 0));
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position - new Vector3(0, 1f, 0), 0.25f);
     }
 }
